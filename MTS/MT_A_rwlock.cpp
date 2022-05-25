3e7b0068caac0c00f59b3d1f0470ecb4a19bd640
番茄_summer <afeism@outlook.com>
2022-05-25 23:20:32 +0800

    new Multi-Thead Sync
    - new RW-Lock Sync

---
+++ MTS/MT_A_rwlock.cpp
@@ -0,0 +1,85 @@
+
+//------------------------------//
+//   Coded by 番茄
+//   @summer studio
+//------------------------------//
+
+
+#include <unistd.h>     // POSIX API (Unix,Linux,masOS)
+#include <pthread.h>    // POSIX Thread
+#include <stdlib.h>     // C Standard Library
+#include <stdio.h>
+
+#define THSCALE   100
+
+using namespace std;
+
+
+// Thread Function Declaration
+void *Thread_Func(void *args);
+
+pthread_rwlock_t g_rwlock;
+int g_count;
+int g_tid[THSCALE];
+int g_thcount[THSCALE];
+
+
+int main(int argc, char **argv, char *env[])
+{    
+    pthread_t tid[THSCALE];
+    //void *status;
+
+	int res = pthread_rwlock_init(&g_rwlock, NULL);
+	if(res != 0)
+	{
+		perror("pthread_rwlock_init() failed \n");
+		exit(EXIT_FAILURE);
+	}
+	else
+		printf("pthread_rwlock_init() suceed \n");
+
+    for (int i = 0; i < 10; i++)
+	{
+        g_count = 0;
+		for (int j = 0; j < THSCALE; j++)
+		{
+			g_tid[j] = j;
+			g_thcount[j] = 0;
+		}
+
+		for (int j = 0; j < THSCALE; j++)
+		{
+			pthread_create(&tid[j], NULL, Thread_Func, (void *)&g_tid[j]);
+		}
+        sleep(2);
+		printf("#%d  g_count = %d \n", i, g_count);
+	}
+
+	res = pthread_rwlock_destroy(&g_rwlock);
+	if(res == 0)
+		printf("pthread_rwlock_destroy suceed() \n");
+
+    printf("Process #%d FINISHED \n", getpid());
+    exit(EXIT_SUCCESS);
+}
+
+void *Thread_Func(void *args)
+{
+	int *tid = (int *)args;
+    for (int i = 0; i < 10000; i++)
+	{
+		g_thcount[*tid]++;
+		//pthread_rwlock_rdlock(&g_rwlock);
+		pthread_rwlock_wrlock(&g_rwlock);
+		g_count++;
+		pthread_rwlock_unlock(&g_rwlock);
+	}
+	//printf("ThreadID: %d Count=%d\n", *tid, g_thcount[*tid]);
+    
+    pthread_exit(NULL);
+}
+
+
+//------------------------------//
+//   River flows in summer
+//------------------------------//
